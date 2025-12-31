33eacdeee12d8e857a81532daabf3779fbef7820
Rusdianaputra17 <rusdianaajoes17@gmail.com>
2025-12-31 20:00:04 +0800

    revisi koneksi database

--- config/database.php
+++ config/database.php
@@ -1,16 +1,26 @@
 <?php
-$host = "localhost";
-$db   = "blog_pribadi";
-$user = "root";
-$pass = "rusdiana87";
-
-try {
-    $conn = new PDO(
-        "mysql:host=$host;dbname=$db;charset=utf8",
-        $user,
-        $pass
-    );
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Koneksi gagal: " . $e->getMessage());
+// database config
+class Database {
+    public $host = "localhost";
+    public $db_name = "blog_pribadi";    // sesuaikan dengan nama database yang sudah dibuat
+    public $username = "root";           // sesuaikan dengan user MySQL
+    public $password = "rusdiana87";     // sesuaikan dengan password MySQL 
+    public $conn;
+     // Fungsi untuk koneksi database
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$this->host};dbname={$this->db_name}",
+                $this->username,
+                $this->password
+            );
+              // Set error mode ke exception
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
 }
+?>
