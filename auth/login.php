32b980050105095135007eb245b98ab8407964b5
Rusdianaputra17 <rusdianaajoes17@gmail.com>
2026-01-03 22:24:10 +0800

    Menambahkan struktur HTML Form Login

--- auth/login.php
+++ auth/login.php
@@ -7,7 +7,7 @@ $conn = $db->getConnection();
 
 if (isset($_POST['login'])) {
     $username = $_POST['username'];
-    $password = md5($_POST['password']);
+    $password = md5($_POST['password']); // sesuai dengan database (MD5)
 
     $query = "SELECT * FROM users WHERE username = :username AND password = :password";
     $stmt = $conn->prepare($query);
@@ -20,10 +20,52 @@ if (isset($_POST['login'])) {
     if ($user) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['username'] = $user['username'];
-        header("Location: ../views/index.php"); 
+        header("Location: ../views/index.php"); // halaman setelah login
         exit;
     } else {
         $error = "Username atau password salah!";
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body class="login-body">
+  <div class="wrapper">
+
+    <!-- SATU FORM SAJA -->
+    <form action="login.php" method="POST">
+      <h1>Login</h1>
+
+      <div class="input-box">
+        <input type="text" name="username" placeholder="Username" required>
+        <i class='bx bxs-user'></i>
+      </div>
+
+      <div class="input-box">
+        <input type="password" name="password" placeholder="Password" required>
+        <i class='bx bxs-lock-alt'></i>
+      </div>
+
+      <div class="remember-forgot">
+        <label><input type="checkbox"> Remember Me</label>
+        <a href="#">Forgot Password</a>
+      </div>
+
+      <button type="submit" name="login" class="btn">Login</button>
+
+      <div class="register-link">
+        <p>Don't have an account? <a href="#">Register</a></p>
+      </div>
+    </form>
+
+  </div>
+</body>
+</html>
