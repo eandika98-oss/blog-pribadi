7c7a13f2886e8cd3185ce11ad2b7a09dea77ff4e
Rusdianaputra17 <rusdianaajoes17@gmail.com>
2026-01-03 21:21:33 +0800

    menambahkan logika pada fitur login

--- auth/login.php
+++ auth/login.php
@@ -4,3 +4,26 @@ require_once __DIR__ . '/../config/database.php';
 
 $db = new Database();
 $conn = $db->getConnection();
+
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+
+    $query = "SELECT * FROM users WHERE username = :username AND password = :password";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':password', $password);
+    $stmt->execute();
+
+    $user = $stmt->fetch();
+
+    if ($user) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        header("Location: ../views/index.php"); 
+        exit;
+    } else {
+        $error = "Username atau password salah!";
+    }
+}
+?>
