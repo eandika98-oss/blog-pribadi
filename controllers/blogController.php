94cb16ffb175b191282da3d46bd9721cdedafe80
ediandika98 <eandika98@gmail.com>
2026-01-06 21:02:17 +0800

    menambahkan method pada class BlogController

--- controllers/blogController.php
+++ controllers/blogController.php
@@ -12,3 +12,29 @@ class BlogController {
     public function __construct($db) {
         $this->model = new BlogModel($db);
     }
+    
+    // Menampilkan seluruh post
+    public function index() {
+        return $this->model->getAll();
+    }
+
+    // Menampilkan satu post berdasarkan ID
+    public function show($id) {
+        return $this->model->getById($id);
+    }
+
+    // Menyimpan post baru
+    public function store($data) {
+        return $this->model->insert($data);
+    }
+
+    // Memperbarui post
+    public function update($id, $data) {
+        return $this->model->update($id, $data);
+    }
+
+    // Menghapus post
+    public function destroy($id) {
+        return $this->model->delete($id);
+    }
+}
